2ba4501bd2d3ce7f761424392e7ce860bc6378e4
Nick Jones <120035708+ndjone12-asu@users.noreply.github.com>
2024-10-04 08:49:06 -0500

    tic-tac-toe with ability to add X to each square

--- src/App.js
+++ src/App.js
@@ -1,3 +1,46 @@
-export default function Square() {
-    return <button className="square">X</button>;
+import { useState } from 'react';
+
+function Square( {value, handleClick} ) {
+  //const [value, setValue] = useState(null);
+
+
+
+  return (
+    <button
+      className="square"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
+}
+
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  function handleClick(i) {
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
   }
+  return (
+    <>
+    <div className="board-row">
+      <Square value={squares[0]} handleClick={() => handleClick(0)}/>
+      <Square value={squares[1]} handleClick={() => handleClick(1)}/>
+      <Square value={squares[2]} handleClick={() => handleClick(2)}/>
+    </div>
+    <div className="board-row">
+      <Square value={squares[3]} handleClick={() => handleClick(3)}/>
+      <Square value={squares[4]} handleClick={() => handleClick(4)}/>
+      <Square value={squares[5]} handleClick={() => handleClick(5)}/>
+    </div>
+    <div className="board-row">
+      <Square value={squares[6]} handleClick={() => handleClick(6)}/>
+      <Square value={squares[7]} handleClick={() => handleClick(7)}/>
+      <Square value={squares[8]} handleClick={() => handleClick(8)}/>
+    </div>
+    </>
+  );
+  }
+
+
