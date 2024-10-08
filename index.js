ed7fec2c8f0b5f5ecacc7c1a75c281060a687fa4
Nick Jones <120035708+ndjone12-asu@users.noreply.github.com>
2024-10-08 18:52:42 -0500

    added additional endpoints

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-   res.send("Hello world!");
+    res.send("This is the home page!");
+ });
+
+app.get('/hello', function(req, res){
+   res.send("Hello World!");
+});
+
+app.post('/hello', function(req, res){
+   res.send("You just called the post method at '/hello'!\n");
 });
 
 app.listen(3000);
